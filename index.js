8fb7e2f6b6057242d82677dfff44bab90175900a
IrineCKimeto <irenekimetoc@gmail.com>
2024-09-27 07:53:59 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = roller-skate){
+return "No argument passed.";
+}
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+function wrapAdjective("Irine")("a dedicated programmer"){
+    return function = "special"
+    return "You are a dedicated programmer"
+}
